60a268ee1c68724b845e31a2982ab872b0fc6c55
Wallace Rafael Máximo Rodrigues <wallace.rodrigues@admin.vixteam>
2020-09-02 13:49:12 -0300

    Criando separação de serviços

---
+++ src/main/java/com/teste/cep/service/CepSevice.java
@@ -0,0 +1,4 @@
+package com.teste.cep.service;
+
+public interface CepSevice {
+}
---
+++ src/main/java/com/teste/cep/service/impl/CepServiceImpl.java
@@ -0,0 +1,8 @@
+package com.teste.cep.service.impl;
+
+import com.teste.cep.service.CepSevice;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CepServiceImpl implements CepSevice {
+}
