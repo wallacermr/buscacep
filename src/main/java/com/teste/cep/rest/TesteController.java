893b472c6c50e9c84c2a8b8aebf62e1ee90c28e9
Wallace Rafael Máximo Rodrigues <wallace.rodrigues@admin.vixteam>
2020-09-02 12:24:58 -0300

    ajustes

--- src/main/java/com/teste/cep/rest/TesteController.java
+++ src/main/java/com/teste/cep/rest/TesteController.java
@@ -7,7 +7,10 @@ import com.teste.cep.entity.Cep;
 import com.teste.cep.repository.CepRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/teste")
