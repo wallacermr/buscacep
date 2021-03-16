79a3d0db9e34edbda6cc294574c0e1aa9eb6435b
wallacermr <wallacermr@gmail.com>
2021-03-16 11:27:24 -0300

    back button name to normal

--- src/main/ui/app/src/components/Botao.js
+++ src/main/ui/app/src/components/Botao.js
@@ -1,7 +1,7 @@
 export function Botao({onClick}) {
     return(
         <div  style={{marginTop: '0.3rem'}}>
-            <button className="btn btn-primary" id="senmmdBtn" onClick={onClick}>Enviar</button>
+            <button className="btn btn-primary" id="sendBtn" onClick={onClick}>Enviar</button>
         </div>
     );
 }
