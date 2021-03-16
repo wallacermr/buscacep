ce855b155174a488a2cd9a79541bc9f2f665757a
wallacermr <wallacermr@gmail.com>
2021-03-16 11:48:45 -0300

    unifying config classes

--- src/main/java/com/teste/cep/config/DefaultViewController.java
+++ src/main/java/com/teste/cep/config/DefaultViewController.java
@@ -1,6 +1,7 @@
 package com.teste.cep.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,10 @@ public class DefaultViewController implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/apirest/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
