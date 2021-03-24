b83b9d5fee1d35965c852997784789e8b8849e94
wallacermr <wallacermr@gmail.com>
2021-03-24 14:38:54 -0300

    add query in the repository

--- src/main/java/com/buscacep/cep/repository/CepRepository.java
+++ src/main/java/com/buscacep/cep/repository/CepRepository.java
@@ -2,7 +2,12 @@ package com.buscacep.cep.repository;
 
 import com.buscacep.cep.entity.Cep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CepRepository extends JpaRepository<Cep, Long> {
-    Cep findByCep(String cep);
+public interface CepRepository extends JpaRepository<Cep, Long>, JpaSpecificationExecutor<Cep> {
+
+    @Query(value = "SELECT c FROM Cep c WHERE c.cep = :cep")
+    Cep findByCep(@Param("cep") String cep);
 }
