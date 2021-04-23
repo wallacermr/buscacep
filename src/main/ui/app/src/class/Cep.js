6249c97ed3e4dbaf431d703547266bcdac4a5c6c
wallacermr <wallacermr@gmail.com>
2021-04-23 11:02:02 -0300

    create a Cep React class

---
+++ src/main/ui/app/src/class/Cep.js
@@ -0,0 +1,14 @@
+export class Cep {
+    constructor(id, cep, logradouro, complemento, bairro, localidade, uf, ibge, gia, ddd) {
+        this.id = id;
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+        this.ibge = ibge;
+        this.gia = gia;
+        this.ddd = ddd;
+    }
+}
